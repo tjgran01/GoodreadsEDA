dbb9280f8ae104c5d422791bcd2e8db863346f6a
tjgran01 <tjgran01@syr.edu>
2018-02-09 11:21:00 -0500

    Began working on data visualization component.

--- GoodreadsEDA/tests.py
+++ GoodreadsEDA/tests.py
@@ -1,54 +1,34 @@
-import csv
+import pandas as pd
+import sqlite3
 import os
-import time
+import datetime
 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
-url = "https://www.goodreads.com/book/show/19486412-big-little-lies"
+cwd = os.getcwd()
 
-# Options for the Chrome driver. Chome operates without calling a window.
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
+conn = sqlite3.connect(f"{cwd}/review_dbs/reviews.db")
 
-driver = webdriver.Chrome(chrome_options=options)
-driver.get(url)
+df = pd.read_sql_query("""SELECT * FROM Reviews
+                          WHERE book_title="Truly Madly Guilty"
+                          """, conn)
 
-html = urlopen(url)
-bsObj = BeautifulSoup(html, "html.parser")
+df["review_date"] = pd.to_datetime(df["review_date"])
+df.set_index("review_date", inplace=True)
+df.sort_index(inplace=True)
 
-for x in range(0, 10):
-
-    reviews = driver.find_elements_by_class_name("reviewHeader")
-
-    for i, review in enumerate(reviews):
-        user = review.find_element_by_class_name("user").text
-        date = review.find_element_by_class_name("reviewDate").text
-        try:
-            score = review.find_element_by_class_name(" staticStars").text
-        except NoSuchElementException as e:
-            print("No Score Given")
-            score = "No Score Given"
-        print(f"{i + 1} | {user} | {date} | {score}")
+df["review_score"] = df["review_score"].apply(pd.to_numeric, errors='coerce')
 
 
+print(df.head())
 
-    print("clicking link!")
-    try:
-        next_link = driver.find_element_by_class_name("next_page")
-        driver.execute_script("arguments[0].click()", next_link)
-        print(driver.find_element_by_class_name("next_page").text)
-        print("sleeping... ...")
-        # after experimenting with a few different sleep times, this
-        # time (3.5) seems to have allowed the page the proper amount of
-        # time to load.
-        time.sleep(3.5)
-    except:
-        print("Could Not Find a link")
-        print("reloading page...")
-        # keep an eye on this. might just need to adjust sleep time.
+# monthly_avg["review_score"] = df["review_score"].resample("M").mean()
+x = df.index
+y = df["review_score"]
 
-    driver.close
+plt.plot(x, y)
+plt.title("Review Scores of 'Truly Madly Guilty' over time.")
+plt.show()
+
+conn.close()
