f9d82e7dd412c92803d3c017d5e51770ecf75db9
tjgran01 <tjgran01@syr.edu>
2018-02-18 16:03:15 -0500

    Created db of applicable movie release dates. Created imdb_release.py. Updated bk_plt.py to reflect the changes in the db structure.

--- GoodreadsEDA/bk_plt.py
+++ GoodreadsEDA/bk_plt.py
@@ -9,25 +9,49 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-pastel')
 import matplotlib.dates as mdates
 
+def get_mv_release(bk_m):
+
+    df_rel = pd.read_sql_query(f"""SELECT * FROM Release_Dates
+                                   WHERE book_title="{bk_m}"
+                                   ;""", conn)
+
+    return df_rel["release_date"][0]
+
+
 n_reviews = 30
 
+movie_titl = ["Big Little Lies", "Me Before You", "The Circle",
+              "Call Me By Your Name", "Everything Everything",
+              "Room", "Brooklyn", "A Man Called Ove",
+              "Miss Peregrine’s Home for Peculiar Children",
+              "The Girl on the Train", "The Light Between Oceans",
+              "The 5th Wave", "Still Alice", "The D.U.F.F.",
+              "Beasts of No Nation", "The Family Fang",
+              "This Is Where I Leave You", "Me and Earl and the Dying Girl",
+              "The Handmaid’s Tale", "The Dinner",
+              "Billy Lynn’s Long Halftime Walk", "Ender’s Game", "If I Stay"]
+
+# Test Cases.
+
 # Test Big Little Lies
-bk = "Big Little Lies"
-mv_release = "2017-02-19"
+# bk = "Big Little Lies"
+# bk_m = movie_titl[0]
 
 # Test Me Before You
 # bk = "Me Before You (Me Before You, #1)"
-# mv_release = "2016-05-23"
+# bk_m = movie_titl[1]
 
 # Test Ender's Game
-# bk = "Ender's Game (Ender's Saga, #1)"
-# mv_release = '2013-11-01'
+bk = "Ender's Game (Ender's Saga, #1)"
+bk_m = movie_titl[-2]
 
+# Get movie release date for movie and df of reviews for the book.
 conn = sqlite3.connect(f"{os.getcwd()}/review_dbs/reviews.db")
-
+mv_release = get_mv_release(bk_m)
 df = pd.read_sql_query(f"""SELECT * FROM Reviews
                           WHERE book_title="{bk}"
-                          """, conn)
+                          ;""", conn)
+conn.close() # db no longer needed.
 
 # turn string dates into datetime objects, and sort the dataframe by date.
 df["review_date"] = pd.to_datetime(df["review_date"])
@@ -57,7 +81,7 @@ plt.xlabel("Year")
 
 # plot release date of movie as a line and label it.
 plt.axvline(x=mv_release, color="#f99f75")
-mv_release_datetime = datetime.strptime(mv_release, "%Y-%m-%d")
+mv_release_datetime = datetime.strptime(mv_release, "%d %B %Y")
 plt.text(mv_release_datetime + timedelta(days=10), 4,
                                          'Movie Release Date',
                                          rotation=90,
@@ -65,7 +89,3 @@ plt.text(mv_release_datetime + timedelta(days=10), 4,
 
 plt.title(f"Review Scores of '{bk}' over time.")
 plt.show()
-
-# QUESTION: Should I close the connection after df is created or wait until
-# the end of the script?
-conn.close()
---
+++ GoodreadsEDA/imdb_release.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import os
+import re
+import sqlite3
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+from imdb import IMDb
+ia = IMDb()
+
+
+def replace_double_quotes(string):
+    """When entering information into the SQL db, there is a chance to get
+    double quote characters within the text. This turns them into single
+    quotes, so they do not escape the insert statement."""
+
+    string = string.replace('"', "'")
+    return(string)
+
+
+conn = sqlite3.connect(f"{os.getcwd()}/review_dbs/reviews.db")
+c = conn.cursor()
+
+try:
+    c.execute(f"""CREATE TABLE Release_Dates (
+                  book_title TEXT(100) NOT NULL,
+                  movie_title TEXT(100) NOT NULL,
+                  release_date TEXT(100) NOT NULL
+                  );""")
+except sqlite3.OperationalError as e:
+    c.execute(f"""DROP TABLE Release_Dates;""")
+    print("table being overwritten")
+    c.execute(f"""CREATE TABLE Release_Dates (
+                  book_title TEXT(100) NOT NULL,
+                  movie_title TEXT(100) NOT NULL,
+                  release_date TEXT(100) NOT NULL
+                  );""")
+
+
+# Options for the Chrome driver, so Chrome operates without calling a window.
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+movie_titl = ["Big Little Lies", "Me Before You", "The Circle",
+              "Call Me By Your Name", "Everything Everything",
+              "Room", "Brooklyn", "A Man Called Ove",
+              "Miss Peregrine’s Home for Peculiar Children",
+              "The Girl on the Train", "The Light Between Oceans",
+              "The 5th Wave", "Still Alice", "The D.U.F.F.",
+              "Beasts of No Nation", "The Family Fang",
+              "This Is Where I Leave You", "Me and Earl and the Dying Girl",
+              "The Handmaid’s Tale", "The Dinner",
+              "Billy Lynn’s Long Halftime Walk", "Ender’s Game", "If I Stay"]
+
+for title in movie_titl:
+    print(title)
+    s_result = ia.search_movie(title)
+    print(s_result[0]['long imdb canonical title'], s_result[0].movieID)
+    mov_title = s_result[0]['long imdb canonical title']
+    mov_ID = s_result[0].movieID
+
+    mov_url = f"http://www.imdb.com/title/tt{mov_ID}/"
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(mov_url)
+    mov_info = driver.find_element_by_id("titleDetails")
+    r_date_elm = mov_info.find_element_by_xpath("//*[contains(text(), 'Release Date:')]/parent::*").text
+    r_date = re.findall(r'\d\d\s(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}', r_date_elm)
+    if not r_date:
+        r_date = re.findall(r'\d\s(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d{4}', r_date_elm)
+
+    try:
+        c.execute(f"""INSERT INTO Release_Dates (
+                      book_title, movie_title,
+                      release_date)
+                      VALUES ("{title}", "{mov_title}",
+                      "{r_date[0]}"
+                      )""")
+    except sqlite3.OperationalError as e:
+        print(f"ERROR: {e}")
+        mov_title = replace_double_quotes(mov_title)
+        c.execute(f"""INSERT INTO Release_Dates (
+                      book_title, movie_title,
+                      release_date)
+                      VALUES ("{title}", "{mov_title}",
+                      "{r_date[0]}"
+                      )""")
+        print("ERROR resolved.")
+
+    conn.commit()
+    driver.close()
+
+conn.close()
